b6cb9a69f7cf697a359286fed659b40d7ea5a6ee
Yohualkis <Yohualkis99@gmail.com>
2025-07-04 17:36:43 -0400

    refactor(models): Se modifico en su totalidad el modelo de tareas para que sea de usuarios

---
+++ src/main/kotlin/com/bacano/WebApi/model/Usuarios.kt
@@ -0,0 +1,17 @@
+package com.bacano.WebApi.model
+
+import jakarta.persistence.*
+import java.util.*
+
+@Entity
+@Table(name = "usuarios")
+data class Usuarios(
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    val usuarioId: Long = 0,
+    val nombre: String = "",
+    val email: String = "",
+    val contrasena: String = "",
+    val fotoPath: String? = "",
+    val fechaRegistro: Date = Date(),
+)
+

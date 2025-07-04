88b657f18a6259830ed72c996fca7ddf5fe065e0
Yohualkis <Yohualkis99@gmail.com>
2025-07-04 17:36:11 -0400

    refactor(repositories): Se modifico en su totalidad el repositorio de tareas para adaptarlos a usuarios

--- src/main/kotlin/com/bacano/WebApi/repositories/TareasRepository.kt
+++
@@ -1,7 +0,0 @@
-package com.bacano.WebApi.repositories
-
-import com.bacano.WebApi.model.Tareas
-import org.springframework.data.jpa.repository.JpaRepository
-
-interface TareasRepository: JpaRepository<Tareas, Int> {
-}

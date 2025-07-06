3066efd943cb586e40bf244229f37ff007c7b66c
Yohualkis <Yohualkis99@gmail.com>
2025-07-05 23:48:04 -0400

    refactor(dto): se modifico el dto para que no devuelva contraseña

--- src/main/kotlin/com/bacano/WebApi/dto/AutorizacionResponseDto.kt
+++ src/main/kotlin/com/bacano/WebApi/dto/AutorizacionResponseDto.kt
@@ -1,8 +1,12 @@
 package com.bacano.WebApi.dto
 
-import com.bacano.WebApi.model.Usuarios
+import java.util.*
 
 data class AutorizacionResponseDto(
     val token: String,
-    val usuario: Usuarios
+    val usuarioId: Long = 0,
+    val nombre: String = "",
+    val email: String = "",
+    val fotoPath: String? = "",
+    val fechaRegistro: Date,
 )

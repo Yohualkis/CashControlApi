f8f12d8fe313b17a104651f6ccc7bb43e21d0250
Yohualkis <Yohualkis99@gmail.com>
2025-07-13 04:19:49 -0400

    refactor(service): se agregaron las funciones crud al service de usuarios

--- src/main/kotlin/com/bacano/WebApi/services/UsuarioService.kt
+++ src/main/kotlin/com/bacano/WebApi/services/UsuarioService.kt
@@ -2,14 +2,33 @@ package com.bacano.WebApi.services
 
 import com.bacano.WebApi.model.Usuario
 import com.bacano.WebApi.repositories.UsuarioRepository
+import org.springframework.security.crypto.password.PasswordEncoder
 import org.springframework.stereotype.Service
 
 @Service
 class UsuarioService(
     private val userRepo: UsuarioRepository,
+    private val encoder: PasswordEncoder,
 ) {
-    fun getAll(): List<Usuario> = userRepo.findAll()
-    fun register(usuario: Usuario): Usuario = userRepo.save(usuario)
-    fun login(email: String, contrasena: String): Usuario? = userRepo.findByEmailAndContrasena(email, contrasena)
-    fun checkEmailExistance(email: String): Boolean = userRepo.findByEmail(email)
+    fun getAll(): List<Usuario> =
+        userRepo.findAll()
+
+    fun createUser(usuario: Usuario): Usuario? {
+        val usuarioExistente = userRepo.findByEmail(usuario.email)
+
+        return if (usuarioExistente == null) {
+            val userConPasswordHasheada = usuario.copy(password = encoder.encode(usuario.password))
+            userRepo.save(userConPasswordHasheada)
+        } else null
+    }
+
+    fun getUserById(id: Long) = userRepo.findByUsuarioId(id)
+    fun getUserByEmail(email: String) = userRepo.findByEmail(email)
+
+    fun deleteUserById(id: Long): Boolean {
+        val userEncontrado = userRepo.findByUsuarioId(id)
+            ?: return false
+        userRepo.delete(userEncontrado)
+        return true
+    }
 }

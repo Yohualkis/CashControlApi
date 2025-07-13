947fd05f9949c58f430782363dc5f079eabb41e8
Yohualkis <Yohualkis99@gmail.com>
2025-07-13 04:15:17 -0400

    feat(config): se crearon las clases que manejan el tema de JWT

---
+++ src/main/kotlin/com/bacano/WebApi/config/Configuration.kt
@@ -0,0 +1,37 @@
+package com.bacano.WebApi.config
+
+import com.bacano.WebApi.repositories.UsuarioRepository
+import com.bacano.WebApi.services.CustomUserDetailsService
+import org.springframework.context.annotation.Configuration
+import org.springframework.boot.context.properties.EnableConfigurationProperties
+import org.springframework.context.annotation.Bean
+import org.springframework.security.authentication.AuthenticationManager
+import org.springframework.security.authentication.AuthenticationProvider
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration
+import org.springframework.security.core.userdetails.UserDetailsService
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
+import org.springframework.security.crypto.password.PasswordEncoder
+
+@Configuration
+@EnableConfigurationProperties(JwtProperties::class)
+class Configuration {
+    @Bean
+    fun userDetailsService(userRepo: UsuarioRepository): UserDetailsService =
+        CustomUserDetailsService(userRepo)
+
+    @Bean
+    fun encoder(): PasswordEncoder = BCryptPasswordEncoder()
+
+    @Bean
+    fun authenticationProvider(userRepo: UsuarioRepository): AuthenticationProvider =
+        DaoAuthenticationProvider()
+            .also {
+                it.setUserDetailsService(userDetailsService(userRepo))
+                it.setPasswordEncoder(encoder())
+            }
+
+    @Bean
+    fun authenticationManager(config: AuthenticationConfiguration): AuthenticationManager =
+        config.authenticationManager
+}

7329da07d0f54ac7ece75e379e173bcbde8a9451
Yohualkis <Yohualkis99@gmail.com>
2025-07-14 02:30:19 -0400

    refactor(categoria): se agrego el usuarioId para poder usarlo como filtro en la BD

--- src/main/kotlin/com/bacano/WebApi/controllers/categoria/CategoriaRequest.kt
+++ src/main/kotlin/com/bacano/WebApi/controllers/categoria/CategoriaRequest.kt
@@ -2,6 +2,7 @@ package com.bacano.WebApi.controllers.categoria
 
 data class CategoriaRequest(
     val categoriaId: Long,
+    val usuarioId: Long,
     val tipoCategoria: String,
     val descripcion: String,
 )

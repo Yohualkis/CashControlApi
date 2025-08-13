5ac477621d65de124cdce421946d8b1e8c8f4885
Yohualkis <Yohualkis99@gmail.com>
2025-08-13 03:12:58 -0400

    feat(repositories): Se elimino el metodo innecesario

--- src/main/kotlin/com/bacano/WebApi/repositories/MetaDetalleRepository.kt
+++ src/main/kotlin/com/bacano/WebApi/repositories/MetaDetalleRepository.kt
@@ -5,8 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.data.jpa.repository.Query
 
 interface MetaDetalleRepository : JpaRepository<MetaDetalle, Long> {
-    fun findByMetaMetaId(metaId: Long): List<MetaDetalle>
-
     @Query("""
         SELECT d FROM MetaDetalle d 
         JOIN FETCH d.meta 
